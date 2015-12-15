f51874540080baa0a592ec271981b7acd888f1cd
Lutz Rosema <terabaud@gmail.com>
2015-12-16 00:18:50 +0100

    removed duplicate comments, more esoteric, vector multiplication

--- regular-polygons/polygons.js
+++ regular-polygons/polygons.js
@@ -3,22 +3,18 @@ Object.getOwnPropertyNames(Math).map(function(p){window[p]=Math[p]})
 
 w=a.width=innerWidth
 h=a.height=innerHeight
-b=document.body
+B=document.body
 c=a.getContext("2d")
 P=[]
 
 // mini vector math lib ;)
-(AP=Array.prototype).xl8=function(a,b,c){for(b=this,c=b.length;b--)b[c]+=a[c];return b}
-AP.rot=function(Φ,a,R){return R=function(v,i,j,d,x,y){x=q[i],y=q[j],q[i]=x*cos(d)+y*sin(d),q[j]=y*cos(d)-x*sin(d),q},a=this,R(a,1,2,Φ[0]),R(a,0,2,Φ[1]),R(a,0,1,Φ[2]),a}
-AP.proj=function(v,k){return v=this,k=.5+v[2]/2e3,[w/2+v[0]*k,h/2+v[1]*k]}
+→=Array.prototype
+→.add=function(a,b,c){for(b=this,c=b.length;c--;)b[c]+=a[c];return b}
+→.mul=function(a,b,c){for(b=this,c=b.length;c--;)b[c]*=a;return b}
+→.rot=function(Φ,a,R){return R=function(v,i,j,d,x,y){x=q[i],y=q[j],q[i]=x*cos(d)+y*sin(d),q[j]=y*cos(d)-x*sin(d),q},a=this,R(a,1,2,Φ[0]),R(a,0,2,Φ[1]),R(a,0,1,Φ[2]),a}
+→.proj=function(v,k){return v=this,k=.5+v[2]/2e3,[w/2+v[0]*k,h/2+v[1]*k]}
 
-
-//n: number of edges
-//r: radius
-//p: center point (vec3)
-//Φ: rotation (vec3) (vim tip: Ctrl+K F*)
-//v: velocity (vec3)
-//ω: rotation velocity (vec3) (vim tip: Crtl+K w*)
+//create a regular n-edged polygon
 function poly(n,r,p,Φ,v,ω,_){
 	_={
 		n:n,r:r, // number of edges and radius
