c00f8a4d3ee8d9b3948db1ab05ec4ed30e40020c
Lutz Rosema <terabaud@gmail.com>
2016-04-07 18:53:43 +0200

    working state

--- interference/interference.js
+++ interference/interference.js
@@ -1,7 +1,9 @@
 with(Math)R=random,Q=sqrt,S=sin,C=cos,A=abs,P=pow
 with(b.style)margin=0,overflow="hidden",backgroundColor='#000'
-W=a.width=innerWidth/4
-H=a.height=innerHeight/4
+with(a.style)width="100vw",height="100vh"
+W=a.width=(innerWidth/2)|0
+H=a.height=(innerHeight/2)|0
+
 ff=255
 // Generate textures
 i1=new ImageData(256,256)
@@ -14,11 +16,11 @@ for(Y=256;Y--;)
 		v1=(Q(x*x+y*y)|0)
 		v2=(127+(S(.03*(A(x)+A(y)))+S(.03*(x*x+y*y))+S(.003*(P(A(x),1.5)+P(A(y),1.5)))*ff/3))|0
 		i1.data[offs  ]=(v1&31)*8
-		i1.data[offs+1]=(v1&63)*4
+		i1.data[offs+1]=(v1&31)
 		i1.data[offs+2]=(v1&15)*16
 		i1.data[offs+3]=ff
 		i2.data[offs  ]=v2
-		i2.data[offs+1]=v2%64
+		i2.data[offs+1]=0
 		i2.data[offs+2]=v2
 		i2.data[offs+3]=ff
 	}
@@ -38,20 +40,7 @@ yω=[.0064,.0045,.0403,.0422]
 I=new ImageData(W,H)
 
 
-
-function Px(x,y,xx,yy,t,i){
-	//test
-	o0=(y*W+x)*4
-	I.data[i0+3]=ff
-	for(i=3;i--;)I.data[o0+i]=t&ff
-	for(i=4;i--;)
-		o1=((xx[i]+x)&ff)+(4*((yy[i]+y)&ff)),
-		I.data[o0  ]+=i1.data[o1  ],
-		I.data[o0+1]+=i1.data[o1+1],
-		I.data[o0+2]+=i1.data[o1+2]
-}
-
-
+function loc(x,y) {return (y*256+x)*4}
 ~function L(t){
 	t/=1e3
 	for(i=4;i--;)
@@ -59,19 +48,15 @@ function Px(x,y,xx,yy,t,i){
 		yy[i]=(C(xφ[i])+1)*128,
 		xφ[i]+=xω[i],yφ[i]+=yω[i]
 	for(y=H;y--;)
-		for(x=W;x--;)
-			Px(x,y,xx,yy,t|0)
-/*
-           for y := 0 to maxy - 1 do
-            for x := 0 to maxx - 1 do
-              plot (x,y) with color 
-                texture1[(x1+x) & 255, (y1+y) & 255] +
-                texture1[(x2+x) & 255, (y2+y) & 255] +
-                texture2[(x3+x) & 255, (y3+y) & 255] +
-                texture2[(x4+x) & 255, (y4+y) & 255] + t;
- 
- 
- */
+		for(x=W;x--;) {
+			dest=(y*W+x)*4
+			o=[]
+			for(i=4;i--;)o[i]=loc(((xx[i]|0)+x)&ff,((yy[i]|0)+y)&ff)
+			I.data[dest  ]=(i1.data[o[0]  ]+i1.data[o[1]  ]+i2.data[o[2]  ]+i2.data[o[3]  ]+t)&ff
+			I.data[dest+1]=(i1.data[o[0]+1]+i1.data[o[1]+1]+i2.data[o[2]+1]+i2.data[o[3]+1]+t)&ff
+			I.data[dest+2]=(i1.data[o[0]+2]+i1.data[o[1]+2]+i2.data[o[2]+2]+i2.data[o[3]+2]+t)&ff
+			I.data[dest+3]=ff
+		}
 	c.putImageData(I,0,0)
 	requestAnimationFrame(L)
 }(0)
