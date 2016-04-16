0b591081f724c8eca06a46dc5af81468858254d4
Lutz Rosema <terabaud@gmail.com>
2016-04-17 01:12:40 +0200

    added mouse events

--- touch-events/touch.js
+++ touch-events/touch.js
@@ -1,16 +1,13 @@
-with(Math)S=sin,C=cos,π=PI,deg=π/180,R=random,Q=sqrt
+with(Math)π=PI
 with(b.style)margin=0,overflow="hidden",backgroundColor='#222'
-a.style.width=a.style.height="100%"
+a.style.width="100vw"
+a.style.height="100vh"
 g=this
 W=a.width=innerWidth
 H=a.height=innerHeight
+m=null
 T=[]
 
-// get distance between 2 points
-function distance(x1, y1, x2, y2) {
-	var dx = x2 - x1, dy = y2 - y1
-	return Q(dx*dx + dy*dy)|0
-}
 g.onresize=setSize=function(){
 	oldH=H
 	W=a.width=innerWidth
@@ -18,7 +15,7 @@ g.onresize=setSize=function(){
 }
 
 function color(i,j){
-	return "hsl("+((((i*18+j*4)|0))%360)+",100%,50%)"
+	return "hsl("+(((((i|0)*36+j*8)|0))%360)+",100%,50%)"
 }
 
 function getTouch(id,i){
@@ -27,52 +24,68 @@ function getTouch(id,i){
 	return -1
 }
 
-g.ontouchstart=function(e,t,o){
+begin=function(o){
+	c.beginPath()
+	c.arc(o.x,o.y,25,0,2*π,false)
+	c.fillStyle=color(o.id, o.n)
+	c.fill()
+}
+
+move=function(o,x,y){
+	c.lineWidth=50
+	c.lineCap="round"
+	c.beginPath()
+	c.moveTo(o.x,o.y)
+	o.x=x
+	o.y=y
+	c.lineTo(o.x,o.y)
+	c.strokeStyle=color(o.id,++o.n)
+	c.stroke()
+}
+
+
+ontouchstart=function(e,t,o){
 	for(t=e.changedTouches,i=t.length;i--;){
-		o={
-			id:t[i].identifier,
-			x:t[i].clientX,
-			y:t[i].clientY,
-			n:0
-		}
-		console.log(o)
-		c.beginPath()
-		c.arc(o.x,o.y,25,0,2*π,false)
-		c.fillStyle=color(o.id, o.n)
-		c.fill()
+		o={id:t[i].identifier,x:t[i].clientX,y:t[i].clientY,n:i*10}
+		begin(o)
 		T.push(o)
 	}
 }
 
-g.ontouchmove=function(e,t,i,j,o){
+ontouchmove=function(e,t,i,j,o){
 	for(t=e.changedTouches,i=t.length;i--;){
 		j=getTouch(t[i].identifier)
 		if(j>-1){
 			o=T[j]
-			c.lineWidth=50
-			c.lineCap="round"
-			c.beginPath()
-			c.moveTo(o.x,o.y)
-			o.x=t[i].clientX
-			o.y=t[i].clientY
-			c.lineTo(o.x,o.y)
-			c.strokeStyle=color(o.id,++o.n)
-			c.stroke()
+			move(o,t[i].clientX,t[i].clientY)
 		}
 	}
 }
 
-
-
-/*
-g.ontouchcancel=function(e,t,i,j){
+ontouchend=function(e,t,i,j){
 	for(t=e.changedTouches,i=t.length;i--;){
 		j=getTouch(t[i].identifier)
 		if(j>-1){
 			T.splice(j,1)
 		}
 	}
-}*/
+}
+
+onmousedown=function(e,o){
+	m={id:"mouse",x:e.clientX,y:e.clientY,n:88}
+}
+
+onmousemove=function(e,o,j){
+	if(!m)return
+	move(m,e.clientX,e.clientY)
+}
+
+onmouseup=function(){
+	m=null
+}
+
+// fading out. If you don't like it, feel free to 
+// fork and comment it out :)
 ~function L(t) {
 	c.fillStyle="rgba(0,0,0,.1)"
 	c.fillRect(0,0,W,H)
