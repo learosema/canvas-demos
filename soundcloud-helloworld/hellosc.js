0d9fde9036e65697f7fa1ac07ba6343b9128d91e
Lutz Rosema <terabaud@gmail.com>
2016-05-27 11:27:17 +0200

    test: do i need this crossOrigin property?

--- soundcloud-helloworld/hellosc.js
+++ soundcloud-helloworld/hellosc.js
@@ -1,10 +1,9 @@
 const trackUrl = "https://soundcloud.com/beatman-and-ludmilla/free-download-beatman-and-ludmilla-breakout-breeze-spring-edition-2016-part-i?in=beatman-and-ludmilla/sets/free-download-beatman-and"
 const clientId = "111c7b71d7cb0701c3cc21e02d37f5e8"
 
-const urlEnc = (params) => 
-	Object.keys(params)
-		.map(p => `${p}=${encodeURIComponent(params[p])}`)
-		.join("&")
+const urlEnc = (params) => Object.keys(params)
+	.map(p => `${p}=${encodeURIComponent(params[p])}`)
+	.join("&")
 
 const http = (url, params) => {
 	const xhr = new XMLHttpRequest()
@@ -23,8 +22,7 @@ http("http://api.soundcloud.com/resolve", {url: trackUrl, client_id: clientId}).
 		const data = JSON.parse(response)
 		console.log("result", data)
 		A.src = data.stream_url + "?" + urlEnc({client_id: clientId})
-		A.crossOrigin = "anonymous"
+		//A.crossOrigin = "anonymous"
 		A.play()
 	})
-
 
