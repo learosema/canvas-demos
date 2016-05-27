2ba57eae86cb9695480ff9ef72d57515e99ebf91
Lutz Rosema <terabaud@gmail.com>
2016-05-27 12:50:38 +0200

    added a basic spectrum analyzer

--- soundcloud-helloworld/hellosc.js
+++ soundcloud-helloworld/hellosc.js
@@ -1,4 +1,4 @@
-const trackUrl = "https://soundcloud.com/beatman-and-ludmilla/free-download-beatman-and-ludmilla-breakout-breeze-spring-edition-2016-part-i?in=beatman-and-ludmilla/sets/free-download-beatman-and"
+const trackUrl = "https://soundcloud.com/beatman-and-ludmilla/free-download-beatman-and-ludmilla-monthly-dj-mix-for-petofi-mr2-radio-april-2016"
 const clientId = "111c7b71d7cb0701c3cc21e02d37f5e8"
 
 const urlEnc = (params) => Object.keys(params)
@@ -17,12 +17,50 @@ const http = (url, params) => {
 	})
 }
 
+const W = C.width = innerWidth
+const H = C.height = innerHeight
+const sound = new AudioContext()
+const source = sound.createMediaElementSource(A)
+const spectrum = sound.createAnalyser()
+spectrum.fftSize = 512
+const buffer = new Uint8Array(spectrum.frequencyBinCount)
+source.connect(sound.destination)
+source.connect(spectrum)
+const c = C.getContext("2d")
+
+const loop = (t) => {
+	c.fillStyle = "rgba(0,0,0,.1)"
+	c.fillRect(0, 0, W, H)
+	spectrum.getByteTimeDomainData(buffer)
+	const dx = W / buffer.length
+	c.beginPath()
+	c.strokeStyle = "#fff"
+	c.moveTo(0, buffer[0]/128.0*H/2)
+	for (let i = 1; i < buffer.length; i++) {
+		c.lineTo(i*dx, buffer[i]/128.0*H/2)
+	}	
+	c.stroke()
+	requestAnimationFrame(loop)
+}
+
+
 http("http://api.soundcloud.com/resolve", {url: trackUrl, client_id: clientId}).then(
 	(response) => {
 		const data = JSON.parse(response)
 		console.log("result", data)
 		A.src = data.stream_url + "?" + urlEnc({client_id: clientId})
-		//A.crossOrigin = "anonymous"
+		A.crossOrigin = "anonymous"
 		A.play()
+		
+		loop(0)
 	})
 
+onkeydown = (e) => {
+	if (e.keyCode == 32) {
+		if (A.paused) {
+			A.play()
+		} else {
+			A.pause()
+		}
+	}
+}
