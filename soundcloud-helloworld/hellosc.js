ab01b88f0228304e2dff28ec4422067a856058a7
Lutz Rosema <terabaud@gmail.com>
2016-05-27 20:12:54 +0200

    onresize event, canvas fit to size

--- soundcloud-helloworld/hellosc.js
+++ soundcloud-helloworld/hellosc.js
@@ -1,6 +1,5 @@
-const trackUrl = "https://soundcloud.com/beatman-and-ludmilla/free-download-beatman-and-ludmilla-monthly-dj-mix-for-petofi-mr2-radio-april-2016"
 const clientId = "111c7b71d7cb0701c3cc21e02d37f5e8"
-
+const trackUrl = "https://soundcloud.com/chris_huelsbeck/techno-dungeon"
 const urlEnc = (params) => Object.keys(params)
 	.map(p => `${p}=${encodeURIComponent(params[p])}`)
 	.join("&")
@@ -11,14 +10,13 @@ const http = (url, params) => {
 	return new Promise((yep, nope) => {
 		xhr.onreadystatechange = () => {
 			if (xhr.readyState != 4) return
-			if (xhr.status == 200) return yep(xhr.responseText)
-			nope(xhr.responseText, xhr.status)
+			(xhr.status == 200 ? yep : nope)(xhr.responseText)
 		}
 	})
 }
 
-const W = C.width = innerWidth
-const H = C.height = innerHeight
+let W = C.width = innerWidth
+let H = C.height = innerHeight
 const sound = new AudioContext()
 const source = sound.createMediaElementSource(A)
 const spectrum = sound.createAnalyser()
@@ -29,7 +27,7 @@ source.connect(spectrum)
 const c = C.getContext("2d")
 
 const loop = (t) => {
-	c.fillStyle = "rgba(0,0,0,.2)"
+	c.fillStyle = "rgba(0,0,0,.1)"
 	c.fillRect(0, 0, W, H)
 	spectrum.getByteTimeDomainData(buffer)
 	const dx = W / buffer.length
@@ -46,19 +44,18 @@ const loop = (t) => {
 	requestAnimationFrame(loop)
 }
 
-
-http("http://api.soundcloud.com/resolve", {url: trackUrl, client_id: clientId}).then(
+const load = (url) => http("http://api.soundcloud.com/resolve", {url: url, client_id: clientId}).then(
 	(response) => {
 		const data = JSON.parse(response)
 		console.log("result", data)
-		A.src = data.stream_url + "?" + urlEnc({client_id: clientId})
-		A.crossOrigin = "anonymous"
-		A.play()
-		
-		loop(0)
+		if (data.stream_url) {
+			A.crossOrigin = "anonymous"
+			A.src = data.stream_url + "?" + urlEnc({client_id: clientId})
+			A.play()
+		}
 	})
 
-onkeydown = (e) => {
+addEventListener('keydown', (e) => {
 	if (e.keyCode == 32) {
 		if (A.paused) {
 			A.play()
@@ -66,4 +63,12 @@ onkeydown = (e) => {
 			A.pause()
 		}
 	}
-}
+})
+
+addEventListener('resize', () => {
+	W = C.width = innerWidth
+	H = C.height = innerHeight
+})
+
+loop(0)
+load(trackUrl)
